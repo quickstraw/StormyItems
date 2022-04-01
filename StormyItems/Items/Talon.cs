25f644d6388390cafc454a1406500b7ddf7756f6
Nathan McGraw <nathanmcgraw64@gmail.com>
2022-04-01 00:35:05 -0500

    Removed redundant code.

--- StormyItems/Items/Talon.cs
+++ StormyItems/Items/Talon.cs
@@ -41,15 +41,6 @@ namespace StormyItems.Items
             CreateLang();
             CreateItem();
 
-            //Add associated buff
-            buffDef = ScriptableObject.CreateInstance<BuffDef>();
-            buffDef.iconSprite = ItemDef.pickupIconSprite;
-            buffDef.name = "Talon";
-            buffDef.isDebuff = false;
-            buffDef.canStack = false;
-            buffDef.buffColor = new Color(246, 255, 71);
-            ContentAddition.AddBuffDef(buffDef);
-
             //But now we have defined an item, but it doesn't do anything yet. So we'll need to define that ourselves.
             RecalculateStatsAPI.GetStatCoefficients += OnGetStatCoefficients;
         }
@@ -82,64 +73,5 @@ namespace StormyItems.Items
             return rules;
         }
 
-        public override void OnFixedUpdate()
-        {
-            if (NetworkServer.active)
-            {
-                ProvideBuff();
-            }
-        }
-
-        public override void OnBodyAdded(RoR2.CharacterBody body)
-        {
-            bool grounded = true;
-            if (body.characterMotor)
-            {
-                grounded = body.characterMotor.isGrounded;
-            }
-            IsGrounded.Add(grounded);
-        }
-
-        public override void OnBodyRemoved(RoR2.CharacterBody body, int index)
-        {
-            if (index < IsGrounded.Count)
-            {
-                IsGrounded.RemoveAt(index);
-            }
-        }
-
-        private void ProvideBuff()
-        {
-            if(!(PlayerCharacterMasterController.instances.Count > 0 && PlayerCharacterMasterController.instances[0].master.GetBody() != null))
-            {
-                return;
-            }
-            for(int i = 0; i < Main.CharBodies.Count; i++)
-            {
-                CharacterBody currChar = Main.CharBodies[i];
-
-                if(!currChar || !currChar.inventory || !currChar.characterMotor)
-                {
-                    continue;
-                }
-
-                int count = GetCount(currChar);
-
-                if (count > 0 && !currChar.characterMotor.isGrounded)
-                {
-                    currChar.AddBuff(buffDef);
-                }
-
-                if (currChar.HasBuff(buffDef))
-                {
-                    if (count <= 0 || currChar.characterMotor.isGrounded)
-                    {
-                        // Set dirty bit to recalculate stats
-                        currChar.MarkAllStatsDirty();
-                        currChar.RemoveBuff(buffDef);
-                    }
-                }
-            }
-        }
     }
 }
